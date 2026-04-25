f8045470674e9ebeb249eaa471c9de73b144ffff
evankgbsn <67293908+evankgbsn@users.noreply.github.com>
2026-04-24 19:29:46 -0500

    ENGINE-1
    - Spatial sounds play but they are not being occluded by geometry

--- GameEngine/FPS/GameObjects/WarehouseBrickWall.cpp
+++ GameEngine/FPS/GameObjects/WarehouseBrickWall.cpp
@@ -4,6 +4,8 @@
 #include "GameEngine/Renderer/Texture/TextureManager.h"
 #include "GameEngine/GameObject/Component/GraphicsObjectTexturedLit.h"
 #include "GameEngine/GameObject/Component/RigidBodyComponent.h"
+#include "GameEngine/Audio/AudioManager.h"
+#include "GameEngine/Audio/AudioObject.h"
 
 WarehouseBrickWall::WarehouseBrickWall() :
 	GameObject("WarehouseBrickWall"),
@@ -24,6 +26,15 @@ void WarehouseBrickWall::Initialize()
 
 	AddComponent(graphics, "Graphics");
 
+	AudioObject::Material mat =
+	{
+		0.03f, 0.04f, 0.07f,
+		0.20f,
+		0.01f, 0.01f, 0.01f
+	};
+
+	audio = AudioManager::CreateAudioObject(GetName(), graphics->GetModel(), graphics->GetPosition(), graphics->GetRotation(), mat);
+
 	body = new RigidBodyComponent(RigidBodyComponent::Type::STATIC, this, graphics->GetModel());
 	body->SyncPhysics();
 
@@ -32,6 +43,8 @@ void WarehouseBrickWall::Initialize()
 
 void WarehouseBrickWall::Terminate()
 {
+	AudioManager::Delete(audio);
+
 	RemoveComponent("RigidBody");
 	delete body;
 
@@ -79,13 +92,13 @@ void WarehouseBrickWall::Unload()
 void WarehouseBrickWall::SetPosition(const glm::vec3& pos)
 {
 	graphics->SetPosition(pos);
-	body->SetPosition(pos);
+	body->SetPosition(pos);audio->SetPosition(pos);
 }
 
 void WarehouseBrickWall::SetRotation(const glm::mat4& rot)
 {
 	graphics->SetRotation(rot);
-	body->SetRotation(rot);
+	body->SetRotation(rot);audio->SetRotation(rot);
 }
 
 glm::vec3 WarehouseBrickWall::GetPosition() const
